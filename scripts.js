14bda442a98231dde19d61b6f6b245e78046fa91
Nusrat Jahan Khan Sristy <nusratjahan08946@gmail.com>
2025-03-22 14:46:40 +0600

    js file

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function showAlert() {
+    alert("Button Clicked!");
+}
+
+function showMessage() {
+    alert("Learn more about our products!");
+}
+
+function addToCart(productName) {
+    alert(productName + " has been added to your cart!");
+}
